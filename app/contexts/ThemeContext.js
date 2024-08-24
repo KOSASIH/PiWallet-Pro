789aa3aa42f1f370f6e74c57381a5e03cb120d83
KOSASIH <kosasihg88@gmail.com>
2024-08-24 20:25:39 +0700

    Create ThemeContext.js

---
+++ app/contexts/ThemeContext.js
@@ -0,0 +1,19 @@
+import { createContext, useState } from 'react';
+
+const ThemeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export { ThemeProvider, ThemeContext };
